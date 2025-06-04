bcff1e334257b3b3f3bbf455cbc4d3b868f14763
ckbxtemplar <kassai.viktor@gmail.com>
2025-06-04 21:05:40 +0200

    update cookeconsent v3

--- app/ui/assets/js/ScriptLoader.tsx
+++ app/ui/assets/js/ScriptLoader.tsx
@@ -8,6 +8,20 @@ import { usePathname } from 'next/navigation';
 
 const GA_MEASUREMENT_ID = 'G-HB1VJFF3ED';
 
+const CAT_NECESSARY = "necessary";
+const CAT_ANALYTICS = "analytics";
+const CAT_ADVERTISEMENT = "advertisement";
+const CAT_FUNCTIONALITY = "functionality";
+const CAT_SECURITY = "security";
+
+const SERVICE_AD_STORAGE = 'ad_storage'
+const SERVICE_AD_USER_DATA = 'ad_user_data'
+const SERVICE_AD_PERSONALIZATION = 'ad_personalization'
+const SERVICE_ANALYTICS_STORAGE = 'analytics_storage'
+const SERVICE_FUNCTIONALITY_STORAGE = 'functionality_storage'
+const SERVICE_PERSONALIZATION_STORAGE = 'personalization_storage'
+const SERVICE_SECURITY_STORAGE = 'security_storage'
+
 declare global {
   interface Window {
     dataLayer: any[];
@@ -17,40 +31,110 @@ declare global {
 
 const ScriptLoader: React.FC = () => {
 
+
 	const locale = useLocale();
   const pathname = usePathname();
-  const [scriptKey, setScriptKey] = useState(0);
-  const [gtagLoaded, setGtagLoaded] = useState(false);
   const isFirstLoad = useRef(true);
+
+	function updateGtagConsent() {
+		console.log("Updating Google Consent Mode with CookieConsent settings");
+
+		window.gtag('consent', 'update', {
+				[SERVICE_ANALYTICS_STORAGE]: CookieConsent.acceptedService(SERVICE_ANALYTICS_STORAGE, CAT_ANALYTICS) ? 'granted' : 'denied',
+				[SERVICE_AD_STORAGE]: CookieConsent.acceptedService(SERVICE_AD_STORAGE, CAT_ADVERTISEMENT) ? 'granted' : 'denied',
+				[SERVICE_AD_USER_DATA]: CookieConsent.acceptedService(SERVICE_AD_USER_DATA, CAT_ADVERTISEMENT) ? 'granted' : 'denied',
+				[SERVICE_AD_PERSONALIZATION]: CookieConsent.acceptedService(SERVICE_AD_PERSONALIZATION, CAT_ADVERTISEMENT) ? 'granted' : 'denied',
+				[SERVICE_FUNCTIONALITY_STORAGE]: CookieConsent.acceptedService(SERVICE_FUNCTIONALITY_STORAGE, CAT_FUNCTIONALITY) ? 'granted' : 'denied',
+				[SERVICE_PERSONALIZATION_STORAGE]: CookieConsent.acceptedService(SERVICE_PERSONALIZATION_STORAGE, CAT_FUNCTIONALITY) ? 'granted' : 'denied',
+				[SERVICE_SECURITY_STORAGE]: CookieConsent.acceptedService(SERVICE_SECURITY_STORAGE, CAT_SECURITY) ? 'granted' : 'denied',
+		});
+	}
 
   // 1. Google Consent Mode: alap denegálás
   useEffect(() => {
+		console.log("Initializing Google Consent Mode ");
+
     window.dataLayer = window.dataLayer || [];
     window.gtag = (...args: any[]) => window.dataLayer.push(args);
 
     window.gtag('consent', 'default', {
-      ad_storage: 'denied',
-      analytics_storage: 'denied',
-      functionality_storage: 'denied',
-      personalization_storage: 'denied',
-      security_storage: 'denied',
-      ad_user_data: 'denied',
-      ad_personalization: 'denied',
+			[SERVICE_AD_STORAGE]: 'denied',
+			[SERVICE_AD_USER_DATA]: 'denied',
+			[SERVICE_AD_PERSONALIZATION]: 'denied',
+			[SERVICE_ANALYTICS_STORAGE]: 'denied',
+			[SERVICE_FUNCTIONALITY_STORAGE]: 'denied',
+			[SERVICE_PERSONALIZATION_STORAGE]: 'denied',
+			[SERVICE_SECURITY_STORAGE]: 'denied',
     });
   }, []);
 
   // 2. CookieConsent futtatása a popup konfigurációddal
   useEffect(() => {
-    const CAT_NECESSARY = 'necessary';
-    const CAT_ANALYTICS = 'analytics';
-    const CAT_ADS = 'ads';
+		console.log("CookieConsent initialized with locale:", locale);
 
     CookieConsent.run({
-      categories: {
-        [CAT_NECESSARY]: { enabled: true, readOnly: true },
-        [CAT_ANALYTICS]: { readOnly: false },
-        [CAT_ADS]: { readOnly: false }
-      },
+			onFirstConsent: () => {
+					updateGtagConsent();
+			},
+			onConsent: () => {
+					updateGtagConsent();
+			},
+			onChange: () => {
+					updateGtagConsent();
+			},      
+			categories: {
+					[CAT_NECESSARY]: {
+							enabled: true,  // this category is enabled by default
+							readOnly: true,  // this category cannot be disabled
+					},
+					[CAT_ANALYTICS]: {
+							autoClear: {
+									cookies: [
+											{
+													name: /^_ga/,   // regex: match all cookies starting with '_ga'
+											},
+											{
+													name: '_gid',   // string: exact cookie name
+											}
+									]
+							},
+							services: {
+									[SERVICE_ANALYTICS_STORAGE]: {
+											label: 'Enables storage (such as cookies) related to analytics e.g. visit duration.',
+									}
+							}
+					},
+					[CAT_ADVERTISEMENT]: {
+							services: {
+									[SERVICE_AD_STORAGE]: {
+											label: 'Enables storage (such as cookies) related to advertising.',
+									},
+									[SERVICE_AD_USER_DATA]: {
+											label: 'Sets consent for sending user data related to advertising to Google.',
+									},
+									[SERVICE_AD_PERSONALIZATION]: {
+											label: 'Sets consent for personalized advertising.',
+									},
+							}
+					},
+					[CAT_FUNCTIONALITY]: {
+							services: {
+									[SERVICE_FUNCTIONALITY_STORAGE]: {
+											label: 'Enables storage that supports the functionality of the website or app e.g. language settings.',
+									},
+									[SERVICE_PERSONALIZATION_STORAGE]: {
+											label: 'Enables storage related to personalization e.g. video recommendations.',
+									},
+							}
+					},
+					[CAT_SECURITY]: {
+							services: {
+									[SERVICE_SECURITY_STORAGE]: {
+											label: 'Enables storage related to security such as authentication functionality, fraud prevention, and other user protection.',
+									},
+							}
+					}
+			},			
 			language: {
 					default: locale,
 					translations: {
@@ -106,73 +190,74 @@ const ScriptLoader: React.FC = () => {
 											closeIconLabel: 'A beállítási ablak bezárása',
 											sections: [
 													{
-															title: 'Valaki sütit említett?',
-															description: 'Kérek egyet!'
+															title: "Cookie usage",
+															description: "We use cookies to ensure the basic functionalities of the website and to enhance your online experience."
 													},
 													{
-															title: 'Szigorúan szükséges sütik',
-															description: 'Ezek a sütik elengedhetetlenek a weboldal megfelelő működéséhez, és nem tilthatók le.',
-															linkedCategory: 'necessary'
+															title: "Strictly necessary cookies",
+															description: "These cookies are essential for the proper functioning of the website, for example for user authentication.",
+															linkedCategory: CAT_NECESSARY,
 													},
 													{
-															title: 'Teljesítmény és analitika',
-															description: 'Ezek a sütik információkat gyűjtenek arról, hogyan használod a weboldalt. Az adatok anonim módon kerülnek rögzítésre, és nem alkalmasak azonosításra.',
-															linkedCategory: 'analytics'
+															title: "Analytics",
+															description: 'Cookies used for analytics help collect data that allows services to understand how users interact with a particular service. These insights allow services both to improve content and to build better features that improve the user’s experience.',
+															linkedCategory: CAT_ANALYTICS,
+															cookieTable: {
+																	headers: {
+																			name: "Name",
+																			domain: "Service",
+																			description: "Description",
+																			expiration: "Expiration"
+																	},
+																	body: [
+																			{
+																					name: "_ga",
+																					domain: "Google Analytics",
+																					description: "Cookie set by <a href=\"https://business.safety.google/adscookies/\">Google Analytics</a>",
+																					expiration: "Expires after 12 days"
+																			},
+																			{
+																					name: "_gid",
+																					domain: "Google Analytics",
+																					description: "Cookie set by <a href=\"https://business.safety.google/adscookies/\">Google Analytics</a>",
+																					expiration: "Session"
+																			}
+																	]
+															}
 													},
 													{
-															title: 'További információ',
-															description: 'Ha kérdésed van a sütikkel kapcsolatos irányelveinkkel vagy választásaiddal kapcsolatban, kérlek <a href="#contact-page">lépj velünk kapcsolatba</a>.'
+															title: 'Advertising',
+															description: 'Google uses cookies for advertising, including serving and rendering ads, personalizing ads (depending on your ad settings at <a href=\"https://g.co/adsettings\">g.co/adsettings</a>), limiting the number of times an ad is shown to a user, muting ads you have chosen to stop seeing, and measuring the effectiveness of ads.',
+															linkedCategory: CAT_ADVERTISEMENT,
+													},
+													{
+															title: 'Functionality',
+															description: 'Cookies used for functionality allow users to interact with a service or site to access features that are fundamental to that service. Things considered fundamental to the service include preferences like the user’s choice of language, product optimizations that help maintain and improve a service, and maintaining information relating to a user’s session, such as the content of a shopping cart.',
+															linkedCategory: CAT_FUNCTIONALITY,
+													},
+													{
+															title: 'Security',
+															description: 'Cookies used for security authenticate users, prevent fraud, and protect users as they interact with a service.',
+															linkedCategory: CAT_SECURITY,
+													},
+													{
+															title: 'More information',
+															description: 'For any queries in relation to the policy on cookies and your choices, please <a href="https://www.example.com/contacts">contact us</a>.'
 													}
 											]
 									}
 							}
 					}
-			},
-      onFirstConsent: updateConsent,
-      onConsent: updateConsent,
-      onChange: updateConsent,
+			}
     });
 
-    function updateConsent() {
-			console.log('Cookie consent updated');
-      const analytics = CookieConsent.acceptedCategory(CAT_ANALYTICS);
-      const ads = CookieConsent.acceptedCategory(CAT_ADS);			
-      window.gtag!(
-        'consent',
-        'update',
-        {
-          ad_storage: ads ? 'granted' : 'denied',
-          analytics_storage: analytics ? 'granted' : 'denied',
-          functionality_storage: CookieConsent.acceptedCategory(CAT_NECESSARY) ? 'granted' : 'denied',
-          personalization_storage: ads ? 'granted' : 'denied',
-          security_storage: CookieConsent.acceptedCategory(CAT_NECESSARY) ? 'granted' : 'denied',
-          ad_user_data: ads ? 'granted' : 'denied',
-          ad_personalization: ads ? 'granted' : 'denied'
-        }
-      );
-      
-      if (CookieConsent.acceptedCategory(CAT_ANALYTICS) && !gtagLoaded) {
-        loadGtag();
-      }
-    }
-
-    function loadGtag() {
-			console.log('Loading Google Analytics script...');
-      const script = document.createElement('script');
-      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
-      script.async = true;
-      script.onload = () => {
-        window.gtag('js', new Date());
-        window.gtag('config', GA_MEASUREMENT_ID, { anonymize_ip: true });
-        setGtagLoaded(true);
-      };
-      document.head.appendChild(script);
-    }
-  }, [locale, gtagLoaded]);
+  }, [locale]);
 
 
-
+	// 3. main.js reinitialization on route change
   useEffect(() => {
+		console.log("Route changed, reloading main.js script");
+		
     if (isFirstLoad.current) {
       // Ez az első betöltés, nem csinálunk semmit, mert Next.js már betöltötte a scriptet
       isFirstLoad.current = false;
